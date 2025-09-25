ef4700276c5328bb47cb98a5accc6be2190a88a9
Arlando A. Ifrene <arlandoifrene@gmail.com>
2025-09-24 23:18:20 -0300

    Add project files.

---
+++ Observer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WeatherStation station = new WeatherStation();
+            station.Attach(new MobileApp());
+            station.Attach(new WebApp());
+
+            station.SetTemperature(25);
+            station.SetTemperature(30);
+
+        }
+    }
+}
---
+++ Observer/WeatherStation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer
+{
+    // Sujeto
+    public class WeatherStation
+    {
+        private List<IObserver> observers = new List<IObserver>();
+        private int temperature;
+
+        public void Attach(IObserver observer) => observers.Add(observer);
+        public void Detach(IObserver observer) => observers.Remove(observer);
+
+        public void SetTemperature(int temp)
+        {
+            temperature = temp;
+            Notify();
+        }
+
+        private void Notify()
+        {
+            foreach (var obs in observers)
+                obs.Update(temperature);
+        }
+    }
+
+    // Observador
+    public interface IObserver
+    {
+        void Update(int temperature);
+    }
+
+    // Observadores concretos
+    public class MobileApp : IObserver
+    {
+        public void Update(int temperature) =>
+            Console.WriteLine($"[Mobile] Nueva temperatura: {temperature}°C");
+    }
+
+    public class WebApp : IObserver
+    {
+        public void Update(int temperature) =>
+            Console.WriteLine($"[Web] Nueva temperatura: {temperature}°C");
+    }
+
+}
